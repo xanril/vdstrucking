1fd17deea9467b4aacc0cd39146a503d9ee47fb9
Kristian Mitra <kristian.m@anino.co>
2017-10-07 15:48:31 +0800

    Initial DB Context class setup

---
+++ VDSTrucking/Data/VDSDBContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using VDSTrucking.Models;
+
+namespace VDSTrucking.Data
+{
+    public class VDSDBContext : DbContext
+    {
+        public VDSDBContext(DbContextOptions<VDSDBContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Truck> Trucks { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Truck>().ToTable("Trucks");
+        }
+    }
+}
