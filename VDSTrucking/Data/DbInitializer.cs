7908c36620c8b667d4d1fc8b776b3cbe038454c2
Kristian Mitra <kristian.m@anino.co>
2017-10-07 16:00:33 +0800

    Create Initial Initializer class

--- VDSTrucking/Data/DbInitializer.cs
+++ VDSTrucking/Data/DbInitializer.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using VDSTrucking.Models;
 
 namespace VDSTrucking.Data
 {
@@ -8,23 +10,16 @@ namespace VDSTrucking.Data
         {
             context.Database.EnsureCreated();
 
-            //// Look for any students.
-            //if (context.Students.Any())
-            //{
-            //    return;   // DB has been seeded
-            //}
+            // Look for any students.
+            if (context.Trucks.Any())
+            {
+                return;   // DB has been seeded
+            }
 
-            //var students = new Student[]
-            //{
-            //new Student{FirstMidName="Carson",LastName="Alexander",EnrollmentDate=DateTime.Parse("2005-09-01")},
-            //new Student{FirstMidName="Meredith",LastName="Alonso",EnrollmentDate=DateTime.Parse("2002-09-01")},
-            //new Student{FirstMidName="Arturo",LastName="Anand",EnrollmentDate=DateTime.Parse("2003-09-01")},
-            //new Student{FirstMidName="Gytis",LastName="Barzdukas",EnrollmentDate=DateTime.Parse("2002-09-01")},
-            //new Student{FirstMidName="Yan",LastName="Li",EnrollmentDate=DateTime.Parse("2002-09-01")},
-            //new Student{FirstMidName="Peggy",LastName="Justice",EnrollmentDate=DateTime.Parse("2001-09-01")},
-            //new Student{FirstMidName="Laura",LastName="Norman",EnrollmentDate=DateTime.Parse("2003-09-01")},
-            //new Student{FirstMidName="Nino",LastName="Olivetto",EnrollmentDate=DateTime.Parse("2005-09-01")}
-            //};
+            var trucks = new Truck[]
+            {
+            new Truck{Name="Truck 486"},
+            };
             //foreach (Student s in students)
             //{
             //    context.Students.Add(s);
