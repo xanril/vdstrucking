531f57b378bd3a2ed61c20a3f914085ab8faa55b
Kristian Mitra <kristian.m@anino.co>
2017-10-07 16:01:05 +0800

    Call the initializer class upon launching

--- VDSTrucking/Program.cs
+++ VDSTrucking/Program.cs
@@ -4,6 +4,9 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using VDSTrucking.Data;
 
 namespace VDSTrucking
 {
@@ -17,6 +20,21 @@ namespace VDSTrucking
                 .UseIISIntegration()
                 .UseStartup<Startup>()
                 .Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<VDSDBContext>();
+                    DbInitializer.Initialize(context);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(new EventId(),ex, "An error occurred while seeding the database.");
+                }
+            }
 
             host.Run();
         }
