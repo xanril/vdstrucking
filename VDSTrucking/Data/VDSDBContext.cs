8acfd0d951c96f6ec52605411aef570319a5d3ed
xanril <motionsmith.ph@gmail.com>
2017-10-08 21:50:39 +0800

    Added Location model

--- VDSTrucking/Data/VDSDBContext.cs
+++ VDSTrucking/Data/VDSDBContext.cs
@@ -13,12 +13,14 @@ namespace VDSTrucking.Data
         public DbSet<Truck> Trucks { get; set; }
         public DbSet<Driver> Drivers { get; set; }
         public DbSet<Helper> Helpers { get; set; }
+        public DbSet<Location> Locations { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Truck>().ToTable("Trucks");
             modelBuilder.Entity<Driver>().ToTable("Drivers");
             modelBuilder.Entity<Helper>().ToTable("Helpers");
+            modelBuilder.Entity<Location>().ToTable("Locations");
         }
     }
 }
---
+++ VDSTrucking/Models/Location.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VDSTrucking.Models
+{
+    public class Location
+    {
+        public int LocationID { get; set; }
+        public string Name { get; set; }
+    }
+}
