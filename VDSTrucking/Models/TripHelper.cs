2841382467013629ff7888ddc5f46b1817e20ad2
xanril <motionsmith.ph@gmail.com>
2017-10-14 19:33:53 +0800

    Create new model TripHelper to associate Helpers for each Trip

---
+++ VDSTrucking/Models/TripHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VDSTrucking.Models
+{
+    public class TripHelper
+    {
+        public int TripHelperID { get; set; }
+
+        [Required]
+        public int TripID { get; set; }
+        public virtual Trip Trip { get; set; }
+
+        [Required]
+        public int HelperID { get; set; }
+        public virtual Helper Helper { get; set; }
+    }
+}
