718d3f387981be1536f445dbaaf5bc36ae572ca7
xanril <motionsmith.ph@gmail.com>
2017-10-15 08:48:25 +0800

    Added initialization of particular items

--- VDSTrucking/Data/DbInitializer.cs
+++ VDSTrucking/Data/DbInitializer.cs
@@ -167,41 +167,34 @@ namespace VDSTrucking.Data
                 new Route{ Location=locations[40], Rate=10000, DriverRate=1000, HelperRate=500}
             };
 
-            //var courses = new Course[]
-            //{
-            //new Course{CourseID=1050,Title="Chemistry",Credits=3},
-            //new Course{CourseID=4022,Title="Microeconomics",Credits=3},
-            //new Course{CourseID=4041,Title="Macroeconomics",Credits=3},
-            //new Course{CourseID=1045,Title="Calculus",Credits=4},
-            //new Course{CourseID=3141,Title="Trigonometry",Credits=4},
-            //new Course{CourseID=2021,Title="Composition",Credits=3},
-            //new Course{CourseID=2042,Title="Literature",Credits=4}
-            //};
-            //foreach (Course c in courses)
-            //{
-            //    context.Courses.Add(c);
-            //}
-            //context.SaveChanges();
+            foreach (Route item in routes)
+            {
+                context.Routes.Add(item);
+            }
+            context.SaveChanges();
 
-            //var enrollments = new Enrollment[]
-            //{
-            //new Enrollment{StudentID=1,CourseID=1050,Grade=Grade.A},
-            //new Enrollment{StudentID=1,CourseID=4022,Grade=Grade.C},
-            //new Enrollment{StudentID=1,CourseID=4041,Grade=Grade.B},
-            //new Enrollment{StudentID=2,CourseID=1045,Grade=Grade.B},
-            //new Enrollment{StudentID=2,CourseID=3141,Grade=Grade.F},
-            //new Enrollment{StudentID=2,CourseID=2021,Grade=Grade.F},
-            //new Enrollment{StudentID=3,CourseID=1050},
-            //new Enrollment{StudentID=4,CourseID=1050},
-            //new Enrollment{StudentID=4,CourseID=4022,Grade=Grade.F},
-            //new Enrollment{StudentID=5,CourseID=4041,Grade=Grade.C},
-            //new Enrollment{StudentID=6,CourseID=1045},
-            //new Enrollment{StudentID=7,CourseID=3141,Grade=Grade.A},
-            //};
-            //foreach (Enrollment e in enrollments)
-            //{
-            //    context.Enrollments.Add(e);
-            //}
+            var particularItems = new ParticularItem[]
+            {
+                new ParticularItem{ Name="PO Diesel"},
+                new ParticularItem{ Name="Diesel"},
+                new ParticularItem{ Name="Toll Fee"},
+                new ParticularItem{ Name="Parking"},
+                new ParticularItem{ Name="Parts"},
+                new ParticularItem{ Name="Penalty"},
+                new ParticularItem{ Name="Terminal / Entrance Fee"},
+                new ParticularItem{ Name="Service Fee"},
+                new ParticularItem{ Name="Porter"},
+                new ParticularItem{ Name="Booking Guard"},
+                new ParticularItem{ Name="Bill of Lading"},
+                new ParticularItem{ Name="Washing Truck"},
+                new ParticularItem{ Name="PPA"},
+                new ParticularItem{ Name="Weight Bridge Fee"},
+                new ParticularItem{ Name="Highway Patrol"},
+                new ParticularItem{ Name="Coast Guard"},
+                new ParticularItem{ Name="Maintenance Expense"},
+                new ParticularItem{ Name="Checker"},
+                new ParticularItem{ Name="Others"}
+            };
         }
     }
 }
